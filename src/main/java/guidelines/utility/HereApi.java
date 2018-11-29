da8a393cdc992ce34fcf680bdd12ea74ffcf644e
TobiasKuch <kuch@hm.edu>
2018-11-29 17:58:27 +0100

    ultilities removed

--- src/main/java/guidelines/utility/HereApi.java
+++
@@ -1,9 +0,0 @@
-package main.java.guidelines.utility;
-
-public class HereApi {
-
-    public static double[] getGeoCodes(String streetName, String cityName, int plz){
-        double[] geocode = new double[] {1,2};
-        return geocode;
-    }
-}
