60f1a33ad64b14fb3bec0323dab62d8acb3a72b9
Denis Angeletta <denisangeletta@dhcp-10-179-8-140.dynamic.eduroam.mwn.de>
2018-11-29 17:00:34 +0100

    First Five Test Classed Done

--- src/test/java/guidelines/handlers/BackToStartIntentHandlerTest.java
+++ src/test/java/guidelines/handlers/BackToStartIntentHandlerTest.java
@@ -1,4 +1,27 @@
 package test.java.guidelines.handlers;
 
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import main.java.guidelines.handlers.BackToStartIntentHandler;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+
 public class BackToStartIntentHandlerTest {
+    private BackToStartIntentHandler handler;
+
+    @Before
+    public void setup(){
+        handler = new BackToStartIntentHandler();
+    }
+
+    @Test
+    public void canHandleTest(){
+        final HandlerInput inputMock = Mockito.mock(HandlerInput.class);
+        when(inputMock.matches(any())).thenReturn(true);
+        assertTrue(handler.canHandle(inputMock));
+    }
 }
--- src/test/java/guidelines/handlers/CancelandStopIntentHandlerTest.java
+++ src/test/java/guidelines/handlers/CancelandStopIntentHandlerTest.java
@@ -1,4 +1,27 @@
 package test.java.guidelines.handlers;
 
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import main.java.guidelines.handlers.CancelandStopIntentHandler;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+
 public class CancelandStopIntentHandlerTest {
+    private CancelandStopIntentHandler handler;
+
+    @Before
+    public void setup(){
+        handler = new CancelandStopIntentHandler();
+    }
+
+    @Test
+    public void canHandleTest(){
+        final HandlerInput inputMock = Mockito.mock(HandlerInput.class);
+        when(inputMock.matches(any())).thenReturn(true);
+        assertTrue(handler.canHandle(inputMock));
+    }
 }
