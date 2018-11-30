0de2334aae54133eacdc10a54147b3de7ceb47a2
TobiasKuch <kuch@hm.edu>
2018-11-30 14:54:44 +0100

    here api dummy

--- src/main/java/guidelines/utilities/hereApi.java
+++ src/main/java/guidelines/utilities/hereApi.java
@@ -1,4 +1,11 @@
 package main.java.guidelines.utilities;
 
+import java.util.Map;
+
 public class hereApi {
+    public static Map<String, Double> getCoordinates(String street, int number, String city, int zip){
+
+        
+        return null;
+    }
 }
