ed5d07ac9d955710bb7137db48932512bd28d62b
TobiasKuch <kuch@hm.edu>
2018-11-30 15:04:29 +0100

    dummy method

--- src/main/java/guidelines/utilities/hereApi.java
+++ src/main/java/guidelines/utilities/hereApi.java
@@ -1,4 +1,9 @@
 package main.java.guidelines.utilities;
 
+import java.util.Map;
+
 public class hereApi {
+    public static Map<String, Double> getCoordinates(String street, int number, String city, int zip){
+        return null;
+    }
 }
