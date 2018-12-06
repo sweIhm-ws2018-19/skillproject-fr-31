9b354a6a70d30a68b1ad561c4cbc0ebe092fcc4c
Denis Angeletta <denisangeletta@dhcp-10-179-10-245.dynamic.eduroam.mwn.de>
2018-12-06 16:51:33 +0100

    Fixed Speech String

--- src/main/java/guidelines/utilities/HereApi.java
+++ src/main/java/guidelines/utilities/HereApi.java
@@ -2,7 +2,7 @@ package main.java.guidelines.utilities;
 
 import java.util.Map;
 
-public class dHereApi {
+public class HereApi {
     public static Map<String, Double> getCoordinates(String street, int number, String city, int zip){
 
         
