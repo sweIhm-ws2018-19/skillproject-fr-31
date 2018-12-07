578a6e6f71587116ca7aaa2ad804c4c7f7bed65f
TobiasKuch <kuch@hm.edu>
2018-12-07 15:03:54 +0100

    Here

---
+++ src/test/java/guidelines/utilities/HereApiTest.java
@@ -0,0 +1,21 @@
+package test.java.guidelines.utilities;
+
+import main.java.guidelines.utilities.Coordinates;
+import main.java.guidelines.utilities.HereApi;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HereApiTest {
+
+    @Test
+    public void getCoordinatesTest(){
+        final Coordinates want = new Coordinates(48.47452, 11.93002);
+        final Coordinates have = HereApi.getCoordinates("In der Feldkirchner Au", 20, "Moosburg", 85368);
+        Assert.assertEquals(want, have);
+    }
+
+    @Test
+    public void getRouteTest(){
+        Assert.assertEquals(true,true);
+    }
+}
