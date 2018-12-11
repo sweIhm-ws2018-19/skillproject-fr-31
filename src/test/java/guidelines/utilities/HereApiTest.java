8fe289a53f389ac96ffe7c784fdb0aadf567998d
TobiasKuch <kuch@hm.edu>
2018-12-11 19:50:11 +0100

    add getNearbyStation with tests

--- src/test/java/guidelines/utilities/HereApiTest.java
+++ src/test/java/guidelines/utilities/HereApiTest.java
@@ -4,6 +4,9 @@ import guidelines.models.Coordinates;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HereApiTest {
 
     @Test
@@ -16,5 +19,14 @@ public class HereApiTest {
     @Test
     public void getRouteTest(){
         Assert.assertEquals(true,true);
+    }
+
+    @Test
+    public void getNearbyStationsTest(){
+        final Map<String, Coordinates> want = new HashMap<>();
+        want.put("Neustadtstraße", new Coordinates(48.4759,11.93533));
+        want.put("Moosburg", new Coordinates(48.470331,11.930385));
+        final Map<String, Coordinates> have = HereApi.getNearbyStations(new Coordinates(48.47452,11.93002));
+        Assert.assertEquals(want,have);
     }
 }
