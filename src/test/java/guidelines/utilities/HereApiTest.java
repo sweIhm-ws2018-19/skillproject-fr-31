37ba30415556fbf51cb6b97b897020c2dbf00b71
Denis Angeletta <denisangeletta@dhcp-10-179-15-127.dynamic.eduroam.mwn.de>
2018-12-11 18:04:47 +0100

    Master into UseCase4

---
+++ src/test/java/guidelines/utilities/HereApiTest.java
@@ -0,0 +1,20 @@
+package guidelines.utilities;
+
+import guidelines.models.Coordinates;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HereApiTest {
+
+    @Test
+    public void getCoordinatesTest(){
+        final Coordinates want = new Coordinates(48.47452, 11.93002);
+        final Coordinates have = HereApi.getCoordinates("In der Feldkirchner Au", 20, "Moosburg", 85368);
+        Assert.assertEquals(want, have);
+    }
+
+    @Test
+    public void getRouteTest(){
+        Assert.assertEquals(true,true);
+    }
+}
