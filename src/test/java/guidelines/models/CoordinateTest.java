be4c11f275375562dbb1222162aa2bcb0d52f6b0
TobiasKuch <kuch@hm.edu>
2018-12-12 18:20:32 +0100

    add CoordinateTest

---
+++ src/test/java/guidelines/models/CoordinateTest.java
@@ -0,0 +1,23 @@
+package guidelines.models;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CoordinateTest {
+
+    @Test
+    public void setLatitudeTest(){
+        final Coordinate have = new Coordinate(1.0, 2.0);
+        have.setLatitude(95.0);
+        final Coordinate want = new Coordinate(95.0 , 2.0);
+        Assert.assertEquals(want, have);
+    }
+
+    @Test
+    public void setLongitudeTest(){
+        final Coordinate have = new Coordinate(1.0, 2.0);
+        have.setLongitude(95.0);
+        final Coordinate want = new Coordinate(1.0 , 95.0);
+        Assert.assertEquals(want, have);
+    }
+}
