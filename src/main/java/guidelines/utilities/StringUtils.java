075ef842315c94789cf369d42e95cf50059cb06e
Sebastian Heusinger <heusings@googlemail.com>
2018-12-19 19:20:59 +0100

    added some breaktimes

--- src/main/java/guidelines/utilities/StringUtils.java
+++ src/main/java/guidelines/utilities/StringUtils.java
@@ -7,9 +7,9 @@ public class StringUtils {
         int lastIndex = stations.size() - 1;
         int count =1;
         for (int i = 0; i < stations.size(); i++) {
-            stations.set(i,(i+1)+". "+stations.get(i));
+            stations.set(i,(i+1)+"<break time=\"0.3s\" />. "+stations.get(i));
         }
-        String result = String.join(" oder ", String.join(", ", stations.subList(0, lastIndex)), stations.get(lastIndex));
+        String result = String.join(" oder <break time=\"0.5s\" />", String.join(", <break time=\"0.5s\" />", stations.subList(0, lastIndex)), stations.get(lastIndex));
         if(stations.size() == 1){
             return stations.get(0);
         }
