f4818b1af626ed0906c34692ee4068c54a170252
TobiasKuch <kuch@hm.edu>
2018-12-21 14:00:46 +0100

    some changes

--- src/test/java/guidelines/utilities/HereApiTest.java
+++ src/test/java/guidelines/utilities/HereApiTest.java
@@ -9,9 +9,7 @@ import java.time.Instant;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 public class HereApiTest {
 
