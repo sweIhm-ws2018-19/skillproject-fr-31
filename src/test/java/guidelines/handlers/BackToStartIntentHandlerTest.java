24342806a49a61b6a42b569354094f438be5a3a9
Denis Angeletta <denisangeletta@localhost.localdomain>
2018-12-21 22:11:31 +0100

    added handleTest for backtostartintent

--- src/test/java/guidelines/handlers/BackToStartIntentHandlerTest.java
+++ src/test/java/guidelines/handlers/BackToStartIntentHandlerTest.java
@@ -2,12 +2,19 @@ package guidelines.handlers;
 
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.handler.RequestHandler;
+import com.amazon.ask.model.Response;
+import guidelines.SpeechStrings;
+import guidelines.statemachine.GuideStates;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
@@ -33,7 +40,25 @@ public class BackToStartIntentHandlerTest {
     }
 
     @Test
-    public void nullHandleTest(){
+    public void nullHandleTest() {
         assertThrows(NullPointerException.class, () -> handler.handle(null));
+    }
+
+    @Test
+    public void handleTest() {
+        final Map<String, Object> sessionAttributes = new HashMap<>();
+        sessionAttributes.put(GuideStates.STATE.getKey(), "test");
+        final Map<String, Object> persistentAttributes = new HashMap<>();
+
+        final HandlerInput inputMock = TestUtil.mockHandlerInput(Collections.emptyMap(), sessionAttributes, persistentAttributes, null);
+        final Optional<Response> res = handler.handle(inputMock);
+
+        assertTrue(res.isPresent());
+        final Response response = res.get();
+
+        assertFalse(response.getShouldEndSession());
+        assertNotEquals("Test", response.getReprompt());
+        assertNotNull(response.getOutputSpeech());
+        assertTrue(response.getOutputSpeech().toString().contains("Du kannst nun wieder die Hilfefunktion aufrufen oder eine Route erfragen"));
     }
 }
