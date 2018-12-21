983a77c3ad8c32dc2c5ce6e706db859527c16962
TobiasKuch <kuch@hm.edu>
2018-12-21 14:06:18 +0100

    delete Test sout

--- src/test/java/guidelines/utilities/HereApiTest.java
+++ src/test/java/guidelines/utilities/HereApiTest.java
@@ -31,10 +31,8 @@ public class HereApiTest {
     public void getRouteTest(){
         final Instant oneHourAgo = Instant.ofEpochMilli(System.currentTimeMillis());
         OffsetDateTime time = oneHourAgo.plusSeconds(2*60*60).atOffset(ZoneOffset.ofHours(1));
-        System.out.println(time);
         final Route have = HereApi.getRoute(new Coordinate(48.14989,11.54617),
                 new Coordinate(48.15427,11.55383),time.toString());
-        System.out.println(have.getMinutesLeft());
         Assert.assertTrue(have.getMinutesLeft() >= 0);
     }
 
